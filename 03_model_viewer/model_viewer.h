715d07fed63b56f53a3bba88d042519c43996d70
Federico Bertani <fedbertani@gmail.com>
2020-08-17 17:43:56 +0200

    working professor code (with added header by me) for project 3

---
+++ 03_model_viewer/model_viewer.h
@@ -0,0 +1,24 @@
+//
+// Created by fede on 17/08/20.
+//
+
+#ifndef COMPUTER_GRAPHICS_MODEL_VIEWER_H
+#define COMPUTER_GRAPHICS_MODEL_VIEWER_H
+
+enum {
+    NAVIGATION,
+    CAMERA_MOVING,
+    TRASLATING,
+    ROTATING,
+    SCALING
+} OperationMode;
+
+enum {
+    X,
+    Y,
+    Z
+} WorkingAxis;
+
+
+
+#endif //COMPUTER_GRAPHICS_MODEL_VIEWER_H
